fc0cdaad05fcd639cc1aea3a0e14d26b4ca3d501
Ayush Nawal <36100944+ayushnawal@users.noreply.github.com>
2018-10-10 22:24:52 +0530

    Add files via upload

--- src/components/counters.js
+++ src/components/counters.js
@@ -2,37 +2,16 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component{
-	state={
-		counters: [
 
-        { id: 1, value: 4},
-        { id: 2, value: 0},
-        { id: 3, value: 0},
-        { id: 4, value: 0}
-		]
-	};
-
-    handleReset = () => {
-         this.state.counters.map(c =>{
-         	c.value=0;
-         	return c;
-         });  
-    };
-
-    handleIncrement = counter => {
-    	console.log(counter);
-    };
-
-	handleDelete = (counterId) => {
-    const counters = this.state.counters.filter( c => c.id !== counterId);
-    this.setState({ counters });
-	};
 
 	render(){
+
+    const { onReset , counters , onDelete , onIncrement } = this.props;
+
 		return(<div>
-			<button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
-            {this.state.counters.map(counter => (
-            <Counter onDelete={this.handleDelete} onIncrement={this.handleIncrement} key={counter.id} counter={counter}>
+			<button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+            {counters.map(counter => (
+            <Counter onDelete={onDelete} onIncrement={onIncrement} key={counter.id} counter={counter}>
             <h4>Counter #{counter.id}</h4>
             </Counter>
             ))}
