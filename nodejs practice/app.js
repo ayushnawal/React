568e2332983d8e9441147a58e42fbd1c3dfb2540
Ayush Nawal <36100944+ayushnawal@users.noreply.github.com>
2018-10-14 23:54:59 +0530

    Add files via upload

--- nodejs practice/app.js
+++ nodejs practice/app.js
@@ -1,9 +1,10 @@
 
-const logger = require('./logger');
+const Logger = require('./logger');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
-
+const EventEmitter = require('events');
+  
 var pathObj = path.parse(__filename);
 var totalMemory = os.totalmem();
 var freeMemory = os.freemem();
@@ -20,4 +21,31 @@ console.log(`Total Memory: ${totalMemory}`);
 console.log(`free Memory: ${freeMemory}`);
 
 console.log(pathObj);
+const logger = new Logger();
+
+logger.on('messageLogged',(arg) =>{
+	console.log('Listener Called',arg);
+});
+
 logger.log('message');
+
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+	if(req.url === '/'){
+		res.write('Hello World');
+		res.end();
+	}
+	if(req.url === '/api/courses'){
+		res.write(JSON.stringify([1,2,3]));
+		res.end();
+	}
+});
+
+//server.on('connection', (socket) => {
+//   console.log('New Connection...')
+//});  
+
+server.listen(3000);
+
+console.log('Listening on port 3000... ');  
